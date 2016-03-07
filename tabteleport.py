0c5706f2eb7bdafe17f030ceee0f2026da1a58ee
Farit Sadykov <erebus-erebus@yandex.ru>
2016-03-07 18:10:20 +0300

    Initial commit

---
+++ tabteleport.py
@@ -0,0 +1,126 @@
+import sublime
+import sublime_plugin
+
+package_name = 'TabTeleport'
+
+
+class TabteleportCtrlTabCommand(sublime_plugin.WindowCommand):
+    """Handler which manages Ctrl+Tab command
+    Creates a new view and runs command to construct list of all open tabs
+    in the view.
+    """
+
+    def run(self):
+        new_view = self.window.new_file()
+        new_view.run_command('construct_tabs_list')
+
+
+class ConstructTabsListCommand(sublime_plugin.TextCommand):
+    """Constructs list of all open tabs in the view"""
+
+    def run(self, edit):
+        self.view.set_scratch(True)
+        self.view.set_name(package_name)
+        self.view.set_syntax_file(
+            'Packages/TabTeleport/tabteleport.tmLanguage')
+
+        tab_list = self._construct_list()
+
+        self.view.insert(edit, 0, ''.join(tab_list + ['\n']))
+        self._set_selection_on_first_line()
+
+        settings = self.view.settings()
+        settings.set('number_of_tabs', len(tab_list))
+
+        self.view.window().focus_view(self.view)
+
+        if 'Vintage' not in settings.get('ignored_packages'):
+            self.view.run_command('exit_insert_mode')
+
+    def _construct_list(self):
+        tab_list = []
+
+        for tab_view in self.view.window().views():
+            if tab_view.file_name() is not None:
+                tab_name = tab_view.file_name().split('/')[-1]
+                tab_list.append(
+                    "{}\n{}\n\n".format(tab_name, tab_view.file_name()))
+
+        return tab_list
+
+    def _set_selection_on_first_line(self):
+        point = self.view.text_point(0, 0)
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(point))
+        self.view.show(point)
+
+
+class SwitchToTabCommand(sublime_plugin.TextCommand):
+    """Handler which opens selected tab """
+
+    def run(self, edit, **kwargs):
+        self.view.window().open_file(kwargs['file_path'])
+
+
+class BlowOutCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, **kwargs):
+        pass
+
+
+class KeyBindingListener(sublime_plugin.EventListener):
+
+    def on_text_command(self, view, command_name, args):
+        if view.name() == package_name:
+            tab_list_nav = TabListNavigaton(view)
+
+            if command_name == 'move' and args['by'] == 'lines':
+                return tab_list_nav.move(forward=args['forward'])
+
+            elif command_name == 'set_motion':
+                if args.get('linewise'):
+                    if args['motion_args']['by'] == 'lines':
+                        return tab_list_nav.move(
+                            forward=args['motion_args']['forward'])
+                else:
+                    return tab_list_nav.switch_to_tab()
+
+            elif command_name == 'insert':
+                return tab_list_nav.switch_to_tab()
+
+            elif command_name in ['switch_to_tab', 'exit_insert_mode']:
+                return None
+
+            else:
+                return ('blow_out_command', {})
+
+    def on_deactivated(self, view):
+        if view.name() == package_name:
+            # GotoTab view has lost input focus
+            view.close()
+
+
+class TabListNavigaton:
+
+    def __init__(self, view):
+        self.view = view
+
+    def switch_to_tab(self):
+        selection_region = self.view.sel()[0]
+        selected_row = self.view.rowcol(selection_region.a)[0]
+        next_row_point = self.view.text_point(selected_row+1, 0)
+        region_line = self.view.full_line(next_row_point)
+        file_path = self.view.substr(region_line).strip().split(": ")[-1]
+        return ('switch_to_tab', {'file_path': file_path})
+
+    def move(self, forward):
+        number_of_tabs = self.view.settings().get('number_of_tabs')
+        selection_region = list(self.view.sel())[0]
+        row = self.view.rowcol(selection_region.a)[0]
+
+        if row == (number_of_tabs * 3 - 3) and forward:
+            return ('blow_out_command', {})
+        else:
+            point = self.view.text_point(row + 2 if forward else row - 2, 0)
+            self.view.sel().clear()
+            self.view.sel().add(sublime.Region(point))
